3d31809a4292a325cac4193978051f32382bb6f1
yayanheeh <tokoxp58@gmail.com>
2019-07-04 23:24:24 +0700

    Update index.jsx

--- src/views/Yayan/index.jsx
+++ src/views/Yayan/index.jsx
@@ -33,7 +33,32 @@ class Yayan extends Component {
         <div className={classes.root}>
          Tessss
  
-Tes
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be
+          {bull}
+          nev
+          {bull}o{bull}
+          lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+
 
         </div>
       </DashboardLayout>
