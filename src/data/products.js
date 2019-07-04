a2404341152b1c801f58d08f22c1a492e7c26b33
yayanheeh <tokoxp58@gmail.com>
2019-07-04 21:42:16 +0700

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -16,7 +16,7 @@ export default [
     description:
       'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
     imageUrl: '/images/products/product_2.png',
-    <a href="/users>Selengkapnya</a> 
+    <router-link to="users">Selengkapnya</router-link>
 totalDownloads: '625',
     price: '27',
     status: 'archived',
