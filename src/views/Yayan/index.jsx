e75b2ae3f8d3c4c81fdeb07ce37f2fe301d696ae
yayanheeh <tokoxp58@gmail.com>
2019-07-05 00:36:05 +0700

    Update index.jsx

--- src/views/Yayan/index.jsx
+++ src/views/Yayan/index.jsx
@@ -32,9 +32,17 @@ class Yayan extends Component {
     return (
       <DashboardLayout title="Icons">
         <div className={classes.root}>
-      Tess 
-
-Tes
+        <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+</Grid>
 
         </div>
       </DashboardLayout>
