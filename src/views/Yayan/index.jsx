fe6503df589f8ac506c8bef1173bedfa69ad17ac
yayanheeh <tokoxp58@gmail.com>
2019-07-04 22:47:36 +0700

    Create index.jsx

---
+++ src/views/Yayan/index.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+// Externals
+import PropTypes from 'prop-types';
+
+// Material helpers
+import { withStyles } from '@material-ui/core';
+
+// Shared layouts
+import { Dashboard as DashboardLayout } from 'layouts';
+
+// Component styles
+const styles = theme => ({
+  root: {
+    padding: theme.spacing.unit * 4
+  },
+  iframe: {
+    width: '100%',
+    minHeight: '640px',
+    border: 0
+  }
+});
+
+class Icons extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <DashboardLayout title="Icons">
+        <div className={classes.root}>
+         Tessss
+ 
+Tes
+
+        </div>
+      </DashboardLayout>
+    );
+  }
+}
+
+Yayan.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Yayan);
