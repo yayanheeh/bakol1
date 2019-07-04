44436a62f352d36346989f90475b792b823fa885
yayanheeh <tokoxp58@gmail.com>
2019-07-04 23:38:40 +0700

    Update index.jsx

--- src/views/Yayan/index.jsx
+++ src/views/Yayan/index.jsx
@@ -30,17 +30,31 @@ class Yayan extends Component {
 
     return (
       <DashboardLayout title="Yayan">
-        <div className={classes.root}>
-         Tessss
- 
-      <Grid item xs={6}>
+       <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={6}>
           <Paper className={classes.paper}>xs=6</Paper>
         </Grid>
         <Grid item xs={6}>
           <Paper className={classes.paper}>xs=6</Paper>
         </Grid>
-Tes
-        </div>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
+    </div>
       </DashboardLayout>
     );
   }
