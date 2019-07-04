f1e8cbb68d8379127db209a50ee5a1de44703850
yayanheeh <tokoxp58@gmail.com>
2019-07-05 00:07:35 +0700

    Update index.jsx

--- src/views/Yayan/index.jsx
+++ src/views/Yayan/index.jsx
@@ -2,78 +2,43 @@ import React, { Component } from 'react';
 
 // Externals
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 // Material helpers
 import { withStyles } from '@material-ui/core';
 
-// Material components
-import { Avatar, Typography, Button, LinearProgress } from '@material-ui/core';
-
-// Shared components
-import { Portlet, PortletContent, PortletFooter } from 'components';
+// Shared layouts
+import { Dashboard as DashboardLayout } from 'layouts';
 
 // Component styles
-import styles from './styles';
+const styles = theme => ({
+  root: {
+    padding: theme.spacing.unit * 4
+  },
+  iframe: {
+    width: '100%',
+    minHeight: '640px',
+    border: 0
+  }
+});
 
 class Yayan extends Component {
   render() {
-    const { classes, className, ...rest } = this.props;
-
-    const rootClassName = classNames(classes.root, className);
+    const { classes } = this.props;
 
     return (
-      <Portlet
-        {...rest}
-        className={rootClassName}
-      >
-        <PortletContent>
-          <div className={classes.details}>
-            <div className={classes.info}>
-              <Typography variant="h2">John Doe</Typography>
-              <Typography
-                className={classes.locationText}
-                variant="body1"
-              >
-                Rm. Valcea, Romania
-              </Typography>
-              <Typography
-                className={classes.dateText}
-                variant="body1"
-              >
-                4:32PM (GMT-4)
-              </Typography>
-            </div>
-            <Avatar
-              className={classes.avatar}
-              src="/images/avatars/avatar_1.png"
-            />
-          </div>
-          <div className={classes.progressWrapper}>
-            <Typography variant="body1">Profile Completeness: 70%</Typography>
-            <LinearProgress
-              value={70}
-              variant="determinate"
-            />
-          </div>
-        </PortletContent>
-        <PortletFooter>
-          <Button
-            className={classes.uploadButton}
-            color="primary"
-            variant="text"
-          >
-            Upload picture
-          </Button>
-          <Button variant="text">Remove picture</Button>
-        </PortletFooter>
-      </Portlet>
+      <DashboardLayout title="Icons">
+        <div className={classes.root}>
+         Tessss
+ 
+Tes
+
+        </div>
+      </DashboardLayout>
     );
   }
 }
 
 Yayan.propTypes = {
-  className: PropTypes.string,
   classes: PropTypes.object.isRequired
 };
 
