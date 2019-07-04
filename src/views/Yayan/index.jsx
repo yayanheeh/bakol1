b045f82a9025f1567f5f3268aa890af09988b4cd
yayanheeh <tokoxp58@gmail.com>
2019-07-04 23:50:00 +0700

    Update index.jsx

--- src/views/Yayan/index.jsx
+++ src/views/Yayan/index.jsx
@@ -1,57 +1,79 @@
 import React, { Component } from 'react';
+
+// Externals
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import { Dashboard as DashboardLayout } from 'layouts';
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
+import classNames from 'classnames';
+
+// Material helpers
+import { withStyles } from '@material-ui/core';
+
+// Material components
+import { Avatar, Typography, Button, LinearProgress } from '@material-ui/core';
+
+// Shared components
+import { Portlet, PortletContent, PortletFooter } from 'components';
+
+// Component styles
+import styles from './styles';
 
 class Yayan extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes, className, ...rest } = this.props;
+
+    const rootClassName = classNames(classes.root, className);
 
     return (
-      <DashboardLayout title="Yayan">
-       <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
-      </DashboardLayout>
+      <Portlet
+        {...rest}
+        className={rootClassName}
+      >
+        <PortletContent>
+          <div className={classes.details}>
+            <div className={classes.info}>
+              <Typography variant="h2">John Doe</Typography>
+              <Typography
+                className={classes.locationText}
+                variant="body1"
+              >
+                Rm. Valcea, Romania
+              </Typography>
+              <Typography
+                className={classes.dateText}
+                variant="body1"
+              >
+                4:32PM (GMT-4)
+              </Typography>
+            </div>
+            <Avatar
+              className={classes.avatar}
+              src="/images/avatars/avatar_1.png"
+            />
+          </div>
+          <div className={classes.progressWrapper}>
+            <Typography variant="body1">Profile Completeness: 70%</Typography>
+            <LinearProgress
+              value={70}
+              variant="determinate"
+            />
+          </div>
+        </PortletContent>
+        <PortletFooter>
+          <Button
+            className={classes.uploadButton}
+            color="primary"
+            variant="text"
+          >
+            Upload picture
+          </Button>
+          <Button variant="text">Remove picture</Button>
+        </PortletFooter>
+      </Portlet>
     );
   }
 }
 
 Yayan.propTypes = {
+  className: PropTypes.string,
   classes: PropTypes.object.isRequired
 };
 
