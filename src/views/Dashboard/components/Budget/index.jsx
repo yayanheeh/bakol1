6fade2d744c7cfef850531758cc8495e5978f2ad
yayanheeh <tokoxp58@gmail.com>
2019-07-05 15:00:20 +0700

    Update index.jsx

--- src/views/Dashboard/components/Budget/index.jsx
+++ src/views/Dashboard/components/Budget/index.jsx
@@ -52,7 +52,7 @@ class Users extends Component {
             </Typography>
           </div>
           <div className={classes.iconWrapper}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/768px-Telegram_logo.svg.png" height="65" width="65" />
+            <img src="http://icons.iconarchive.com/icons/martz90/circle/256/youtube-icon.png" height="65" width="65" />
           </div>
         </div>
         <div className={classes.footer}>
