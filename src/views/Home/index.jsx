02df0749ec5e27375acb582e65efdf22d6c47462
yayanheeh <tokoxp58@gmail.com>
2019-07-05 12:53:07 +0700

    Update index.jsx

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
 
 // Externals
 import PropTypes from 'prop-types';
+
+// Material helpers
+import { withStyles } from '@material-ui/core';
+
+// Shared layouts
+import { Dashboard as DashboardLayout } from 'layouts';
 
 // Component styles
 const styles = theme => ({
@@ -24,32 +31,23 @@ class Home extends Component {
     const { classes } = this.props;
 
     return (
+      <DashboardLayout title="Bakolscript">
         <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
      
+
+<div>
+      <Paper className={classes.root}>
+        <Typography variant="h5" component="h3">
+          This is a sheet of paper.
+        </Typography>
+        <Typography component="p">
+          Paper can be used to build surface or other elements for your application.
+        </Typography>
+      </Paper>
+    </div>
+
+</div>
+      </DashboardLayout>
     );
   }
 }
