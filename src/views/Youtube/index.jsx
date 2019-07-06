ff0918a59dbf58689af5cd7ea3c6559d9c29b6b4
yayanheeh <tokoxp58@gmail.com>
2019-07-06 15:15:22 +0700

    Update index.jsx

--- src/views/Youtube/index.jsx
+++ src/views/Youtube/index.jsx
@@ -1,79 +1,306 @@
 import React, { Component } from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-
+import { Link, withRouter } from 'react-router-dom';
 
 // Externals
 import PropTypes from 'prop-types';
+import compose from 'recompose/compose';
+import validate from 'validate.js';
+import _ from 'underscore';
 
 // Material helpers
 import { withStyles } from '@material-ui/core';
 
-// Shared layouts
-import { Dashboard as DashboardLayout } from 'layouts';
+// Material components
+import {
+  Grid,
+  Button,
+  IconButton,
+  CircularProgress,
+  TextField,
+  Typography
+} from '@material-ui/core';
+
+// Material icons
+import { ArrowBack as ArrowBackIcon } from '@material-ui/icons';
+
+// Shared components
+import { Facebook as FacebookIcon, Google as GoogleIcon } from 'icons';
 
 // Component styles
-const styles = theme => ({
-  root: {
-    padding: theme.spacing.unit * 4
-  },
-  iframe: {
-    width: '100%',
-    minHeight: '640px',
-    border: 0
-  },
-button {
-  background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-}
-});
+import styles from './styles';
 
-class Youtube extends Component {
+// Service methods
+const signIn = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(true);
+    }, 1500);
+  });
+};
+
+class SignIn extends Component {
+  state = {
+    values: {
+      email: '',
+      password: ''
+    },
+    touched: {
+      email: false,
+      password: false
+    },
+    errors: {
+      email: null,
+      password: null
+    },
+    isValid: false,
+    isLoading: false,
+    submitError: null
+  };
+
+  handleBack = () => {
+    const { history } = this.props;
+
+    history.goBack();
+  };
+
+  validateForm = _.debounce(() => {
+    const { values } = this.state;
+
+    const newState = { ...this.state };
+    const errors = validate(values, schema);
+
+    newState.errors = errors || {};
+    newState.isValid = errors ? false : true;
+
+    this.setState(newState);
+  }, 300);
+
+  handleFieldChange = (field, value) => {
+    const newState = { ...this.state };
+
+    newState.submitError = null;
+    newState.touched[field] = true;
+    newState.values[field] = value;
+
+    this.setState(newState, this.validateForm);
+  };
+
+  handleSignIn = async () => {
+    try {
+      const { history } = this.props;
+      const { values } = this.state;
+
+      this.setState({ isLoading: true });
+
+      await signIn(values.email, values.password);
+
+      localStorage.setItem('isAuthenticated', true);
+
+      history.push('/dashboard');
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        serviceError: error
+      });
+    }
+  };
+
   render() {
     const { classes } = this.props;
+    const {
+      values,
+      touched,
+      errors,
+      isValid,
+      submitError,
+      isLoading
+    } = this.state;
+
+    const showEmailError = touched.email && errors.email;
+    const showPasswordError = touched.password && errors.password;
 
     return (
-      <DashboardLayout title="Icons">
-        <div className={classes.root}>
-     
-
-<div>
-      <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
-          This is a sheet of paper.
-        </Typography>
-        <Typography component="p">
-          Paper can be used to build surface or other elements for your application.
-        </Typography>
-      </Paper>
-    </div>
-
-
-
-<Grid container spacing={3}>
- <Grid item xs={12} sm={6}>
-      tes1  </Grid>
-        <Grid item xs={12} sm={6}>
-         tes2 </Grid></Grid>
-
-</div>
-      </DashboardLayout>
+      <div className={classes.root}>
+        <Grid
+          className={classes.grid}
+          container
+        >
+          <Grid
+            className={classes.quoteWrapper}
+            item
+            lg={5}
+          >
+            <div className={classes.quote}>
+              <div className={classes.quoteInner}>
+                <Typography
+                  className={classes.quoteText}
+                  variant="h1"
+                >
+                  Hella narwhal Cosby sweater McSweeney's, salvia kitsch before
+                  they sold out High Life.
+                </Typography>
+                <div className={classes.person}>
+                  <Typography
+                    className={classes.name}
+                    variant="body1"
+                  >
+                    Takamaru Ayako
+                  </Typography>
+                  <Typography
+                    className={classes.bio}
+                    variant="body2"
+                  >
+                    Manager at inVision
+                  </Typography>
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid
+            className={classes.content}
+            item
+            lg={7}
+            xs={12}
+          >
+            <div className={classes.content}>
+              <div className={classes.contentHeader}>
+                <IconButton
+                  className={classes.backButton}
+                  onClick={this.handleBack}
+                >
+                  <ArrowBackIcon />
+                </IconButton>
+              </div>
+              <div className={classes.contentBody}>
+                <form className={classes.form}>
+                  <Typography
+                    className={classes.title}
+                    variant="h2"
+                  >
+                    Sign in
+                  </Typography>
+                  <Typography
+                    className={classes.subtitle}
+                    variant="body1"
+                  >
+                    Sign in with social media
+                  </Typography>
+                  <Button
+                    className={classes.facebookButton}
+                    color="primary"
+                    onClick={this.handleSignIn}
+                    size="large"
+                    variant="contained"
+                  >
+                    <FacebookIcon className={classes.facebookIcon} />
+                    Login with Facebook
+                  </Button>
+                  <Button
+                    className={classes.googleButton}
+                    onClick={this.handleSignIn}
+                    size="large"
+                    variant="contained"
+                  >
+                    <GoogleIcon className={classes.googleIcon} />
+                    Login with Google
+                  </Button>
+                  <Typography
+                    className={classes.sugestion}
+                    variant="body1"
+                  >
+                    or login with email address
+                  </Typography>
+                  <div className={classes.fields}>
+                    <TextField
+                      className={classes.textField}
+                      label="Email address"
+                      name="email"
+                      onChange={event =>
+                        this.handleFieldChange('email', event.target.value)
+                      }
+                      type="text"
+                      value={values.email}
+                      variant="outlined"
+                    />
+                    {showEmailError && (
+                      <Typography
+                        className={classes.fieldError}
+                        variant="body2"
+                      >
+                        {errors.email[0]}
+                      </Typography>
+                    )}
+                    <TextField
+                      className={classes.textField}
+                      label="Password"
+                      name="password"
+                      onChange={event =>
+                        this.handleFieldChange('password', event.target.value)
+                      }
+                      type="password"
+                      value={values.password}
+                      variant="outlined"
+                    />
+                    {showPasswordError && (
+                      <Typography
+                        className={classes.fieldError}
+                        variant="body2"
+                      >
+                        {errors.password[0]}
+                      </Typography>
+                    )}
+                  </div>
+                  {submitError && (
+                    <Typography
+                      className={classes.submitError}
+                      variant="body2"
+                    >
+                      {submitError}
+                    </Typography>
+                  )}
+                  {isLoading ? (
+                    <CircularProgress className={classes.progress} />
+                  ) : (
+                    <Button
+                      className={classes.signInButton}
+                      color="primary"
+                      disabled={!isValid}
+                      onClick={this.handleSignIn}
+                      size="large"
+                      variant="contained"
+                    >
+                      Sign in now
+                    </Button>
+                  )}
+                  <Typography
+                    className={classes.signUp}
+                    variant="body1"
+                  >
+                    Don't have an account?{' '}
+                    <Link
+                      className={classes.signUpUrl}
+                      to="/sign-up"
+                    >
+                      Sign up
+                    </Link>
+                  </Typography>
+                </form>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
 
-Youtube.propTypes = {
-  classes: PropTypes.object.isRequired
+SignIn.propTypes = {
+  className: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Youtube);
+export default compose(
+  withRouter,
+  withStyles(styles)
+)(SignIn);
