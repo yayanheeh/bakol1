eee6ba908cee989e79399e40fb33ed07dbec7af0
yayanheeh <tokoxp58@gmail.com>
2019-07-08 12:25:50 +0700

    Update index.jsx

--- src/views/Youtube/components/Notifications/index.jsx
+++ src/views/Youtube/components/Notifications/index.jsx
@@ -40,20 +40,18 @@ class Notifications extends Component {
             title="Notifications"
           />
         </PortletHeader>
-        <PortletContent noPadding>
-<p>&ensp;  <b>Telegram</b></p><br/>
+        <PortletContent noPadding><br/>p>&ensp;  <b>Telegram</b></p><br/>
 
 <PortletLabel
             subtitle="
                       Sebuah aplikasi layanan pengirim pesan instan multiplatform berbasis awan yang bersifat gratis dan nirlaba. Klien Telegram tersedia untuk perangkat telepon seluler dan sistem perangkat komputer. Para pengguna dapat mengirim pesan dan bertukar foto, video, stiker, audio, dan tipe berkas lainnya."/>  <br/>
 
-<p>&ensp;  <b>Telegram</b></p><br/>
 <p>&ensp;  <b>SYNCED</b></p><br/>
 
 <PortletLabel
             subtitle="
                        Anda dapat mengakses pesan Anda dari semua perangkat sekaligus. Mulailah mengetik di ponsel Anda dan selesaikan pesan dari tablet atau laptop Anda. Jangan pernah kehilangan data Anda lagi."/>  <br/>
-<p>&ensp;  <b>Tanpa batas</b></p><br/>
+ <br/><p>&ensp;  <b>Tanpa batas</b></p><br/>
 
 <PortletLabel
             subtitle="   Anda dapat mengirim media dan file, tanpa batasan pada jenis dan ukurannya. Seluruh riwayat obrolan Anda tidak memerlukan ruang disk pada perangkat Anda, dan akan disimpan dengan aman di cloud Telegram selama Anda membutuhkannya."/>
@@ -61,7 +59,7 @@ class Notifications extends Component {
 
 <PortletLabel
             subtitle="Memberikan keamanan terbaik yang dikombinasikan dengan kemudahan penggunaan. Semua yang ada di Telegram, termasuk obrolan, grup, media, dll. Dienkripsi menggunakan kombinasi enkripsi AES 256-bit simetris, enkripsi RSA 2048-bit, dan pertukaran kunci aman Diffie-Hellman."/>
-<p>&ensp;  <b>Kuat</b></p><br/>
+<p>&ensp;<br/>  <b>Kuat</b></p><br/>
 
 <PortletLabel
             subtitle="Anda dapat membuat obrolan grup hingga 200.000 anggota, berbagi video besar, dokumen apa pun (.DOC, .MP3, .ZIP, dll.), Dan bahkan mengatur bot untuk tugas tertentu. Ini adalah alat yang sempurna untuk hosting komunitas online dan mengoordinasikan kerja tim."/>
@@ -69,19 +67,19 @@ class Notifications extends Component {
 
 <PortletLabel
             subtitle="Dibangun untuk mengirimkan pesan Anda dalam byte minimum yang memungkinkan, Telegram adalah sistem pengiriman pesan paling andal yang pernah dibuat. Ia bekerja bahkan pada koneksi seluler terlemah."/>
-<p>&ensp;  <b>Fun</b></p><br/>
+<p>&ensp;<br/>  <b>Fun</b></p><br/>
 
 <PortletLabel
             subtitle=" Telegram memiliki alat pengeditan foto dan video yang kuat dan platform stiker / GIF terbuka untuk memenuhi semua kebutuhan ekspresif Anda."/>
-<p>&ensp;  <b>Sederhana</b></p><br/>
+<p>&ensp; <br/> <b>Sederhana</b></p><br/>
 
 <PortletLabel
             subtitle="Sementara memberikan berbagai fitur yang belum pernah terjadi sebelumnya, kami sangat berhati-hati untuk menjaga antarmuka tetap bersih. Dengan desain minimalis, Telegram ramping dan mudah digunakan."/>
-<p>&ensp;  <b>Rahasia</b></p><br/>
+<p>&ensp; <br/> <b>Rahasia</b></p><br/>
 
 <PortletLabel
             subtitle=" Menjaga privasi Anda dengan serius dan tidak akan pernah memberikan akses pihak ketiga ke data Anda."/>
-<p>&ensp;  <b>100% gratis tanpa iklan</b></p><br/>
+<p>&ensp;<br/>  <b>100% gratis tanpa iklan</b></p><br/>
 <PortletLabel
             subtitle="Telegram gratis dan akan selalu gratis. Kami tidak akan menjual iklan atau memperkenalkan biaya berlangganan."/>
          
