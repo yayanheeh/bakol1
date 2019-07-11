ca631fa16f87867e7223ac3ae89fb4c95650c305
yayanheeh <tokoxp58@gmail.com>
2019-07-11 12:57:54 +0700

    Update index.jsx

--- src/views/Youtube/components/Password/index.jsx
+++ src/views/Youtube/components/Password/index.jsx
@@ -1,4 +1,32 @@
 import React, { Component } from 'react';
+import CardMedia from '@material-ui/core/CardMedia';
+
+//
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+  }),
+);
+
 
 // Externals
 import PropTypes from 'prop-types';
